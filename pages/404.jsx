f82b82ef13579c393d1e2aba31ef105e7f410427
Victor Hugo <viiict0r@hotmail.com>
2020-12-06 02:18:42 -0300

    chore(styles): 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
+import { Button } from 'antd';
+
+import '../styles/pages/404.less';
 
 function NotFound() {
-  return <h3>Not found</h3>;
+  return (
+    <div className="notfound">
+      <div>
+        <h1>404</h1>
+        <p>O conteúdo que você procura não existe.</p>
+        <Link href="/">
+          <Button type="link">Voltar ao início</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
